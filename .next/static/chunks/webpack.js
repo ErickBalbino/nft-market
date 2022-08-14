fd4f9fdd65fb459a94b39c03396ffecff11f0887
Erick <erickbaddog2@gmail.com>
2022-08-14 12:37:33 -0300

    added navbar

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "f853e6b9af568284"; }
+/******/ 		__webpack_require__.h = function() { return "9a6f6e79b53479b5"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
