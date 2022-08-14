b4a135b25e93728b74a4c9e38c9e134a8538a59a
Erick <erickbaddog2@gmail.com>
2022-08-14 13:23:16 -0300

    added listpage

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "9a6f6e79b53479b5"; }
+/******/ 		__webpack_require__.h = function() { return "2279af3a54c6d662"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
