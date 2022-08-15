7f64c88bebaf6f4b48a5c92361df2898531fc18d
Erick <erickbaddog2@gmail.com>
2022-08-14 22:28:52 -0300

    added list and nft components

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "2279af3a54c6d662"; }
+/******/ 		__webpack_require__.h = function() { return "22fdffa0e8275e7c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
