05e89d584479bedaf8308a70b4a7ff7780698ab3
erick <eriickdeveloper@gmail.com>
2023-01-17 22:13:16 -0300

    mint token

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -136,7 +136,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "3d84be375e1555e6"; }
+/******/ 		__webpack_require__.h = function() { return "5f0c0d3e56752cab"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
